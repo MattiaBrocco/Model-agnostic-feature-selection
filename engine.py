2005b0f7652e9e58468cb997f73ef0eb37538c04
MattiaBrocco <mattia.brocco@gmail.com>
2022-10-31 17:32:17 +0100

    Try: pruned tree perm. importance + likelihood test + benchmark 4 models

---
+++ engine.py
@@ -0,0 +1,209 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.inspection import permutation_importance
+from sklearn.linear_model import LogisticRegressionCV
+
+from tensorflow.keras.utils import to_categorical
+
+
+import support
+
+
+class Classification:
+    
+    def prepare_data(self, data_dir, target):
+        """
+        Parameters
+        ----------
+        
+        data_dir: str. directory in which data is located
+        target : str. columns in 'data' that contains the
+                 values onto which prediction is made.
+        
+        Returns
+        -------
+        
+        X_train, X_test, y_train, y_test
+        """
+        self.target = target
+        self.data_dir = data_dir
+        
+        if ".xlsx" in data_dir:
+            data_import = pd.read_excel(data_dir)
+        else:
+            data_import = pd.read_csv(data_dir)
+
+        if data_import.shape[1] == 1:
+            df = pd.DataFrame([list(data_import.applymap(lambda s: s.split(";")).values)[i][0]
+                               for i in range(len(data_import))])
+            df.columns = [new_c.strip() for new_c in
+                          data_import.columns[0].split(";")]
+
+            for c in df.columns:
+                try:
+                    df[c] = df[c].astype(int)
+                except:
+                    continue
+
+        else:
+            df = data_import.copy()
+        
+        # Perform train-test split
+        X = df.drop(target, axis = 1)
+        y = df[target]
+        y = y.apply(lambda v: 1 if v == y.unique()[0] else 0) 
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = .7,
+                                                            random_state = 42, shuffle = True)
+        # Scale data
+        scaler = MinMaxScaler()
+        scaler.fit(X_train)
+        
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
+        
+        
+        return X_train, X_test, y_train, y_test
+    
+    
+    def variable_selection(self, X_train, X_test, y_train, y_test):
+        """
+        Parameters
+        ----------
+        
+        X_test : 'pandas.DataFrame' or 'numpy.ndarray'
+        y_test : 'pandas.DataFrame' or 'numpy.ndarray'
+        X_train : 'pandas.DataFrame' or 'numpy.ndarray'
+        y_train : 'pandas.DataFrame' or 'numpy.ndarray'
+        """
+        self.X_test = X_test
+        self.y_test = y_test
+        self.X_train = X_train
+        self.y_train = y_train
+        
+        # STEP 1: Permutation importance with a post-pruned tree.
+        #         The tree is subjected to post-pruning (with CV)
+        
+        # 1.1 Pruning
+        pruned_tree = support.D3_pruning(X_train, y_train)
+        
+        # 1.3 Feature selection
+        perm_imp = permutation_importance(pruned_tree, X_test, y_test,
+                                          n_repeats = 30, random_state = 42)
+        
+        selected_features = []        
+        for i in perm_imp.importances_mean.argsort()[::-1]:
+            if perm_imp.importances_mean[i] - 2 * perm_imp.importances_std[i] > 0:
+                
+                selected_features += [i]
+        
+        if len(selected_features) == 0:
+            selected_features = np.where(perm_imp.importances_mean != 0)[0]
+            if len(selected_features) == 1:
+                selected_features = [selected_features]
+            elif len(selected_features) > 1:
+                selected_features = list(selected_features)
+        
+                #print("{} {:.3f} ± {:.3f}".format(X.columns[i],
+                #                                  r2.importances_mean[i],
+                #                                  r2.importances_std[i]))
+
+        # STEP 2: Validation of feature selection. This is made
+        #         By running a (generalized) linear model with
+        #         all the features and the selected features.
+        #         Then with a statistical test, if the full model
+        #         is no better than the restricted model the validation is done.
+        logreg_red = LogisticRegressionCV(Cs = 50, penalty = "l1", random_state = 42,
+                                          n_jobs = -1, solver = "saga", max_iter = 5e3)
+        logreg_red.fit(X_train[:,selected_features], y_train)
+        
+        logreg_full = LogisticRegressionCV(Cs = 50, penalty = "l1", random_state = 42,
+                                           n_jobs = -1, solver = "saga", max_iter = 5e3)
+        logreg_full.fit(X_train, y_train)
+        
+        # 2.1 Run test
+        validation_test = support.likelihood_ratio_test(logreg_red, logreg_full,
+                                                        X_train[:,selected_features],
+                                                        X_train, y_train)
+        # 2.2 Consider factor loading
+        data_fl = pd.DataFrame(np.c_[X_train[:,selected_features],
+                                     y_train]).corr()
+        fl = abs(data_fl).min().min()
+        
+        valid_output = False
+        # validation_test[1]: p-value of likelihood ratio test
+        if validation_test[1] > 0.05:
+            if fl >= 0.7:
+                valid_output = True
+        else:
+            print("Warning: likelihood ratio test H0 should be rejected")
+            
+        return {"Features": selected_features,
+                "Validation passed": valid_output}
+    
+    def benchmark_models(self, X_train, X_test, y_train, y_test, features):
+        """
+        Parameters
+        ----------
+        
+        features : 'dict'. output of 'variable_selection'
+        X_train, X_test, y_train, y_test : output of 'prepare_data'
+        """
+        self.X_test = X_test
+        self.y_test = y_test
+        self.X_train = X_train
+        self.y_train = y_train
+        self.features = features
+        
+        # Logistic regression
+        model1 = LogisticRegression(random_state = 101)
+        model1.fit(X_train[:, features["Features"]], y_train)
+        
+        # Support vector machine
+        model2 = SVC()
+        model2.fit(X_train[:, features["Features"]], y_train)
+
+        # Random forest
+        model3 = RandomForestClassifier(random_state = 101,
+                                        max_depth = len(X_train)/2)
+        model3.fit(X_train[:, features["Features"]], y_train)
+        
+        # Neural network
+        y_train_cat = to_categorical(y_train, 2)
+        y_test_cat = to_categorical(y_test, 2)
+        
+        model4 = support.build_MLP(X_train, y_train_cat, features)
+        
+        
+        # SCORES
+        out = pd.Series( [accuracy_score(y_test,
+                                         model1.predict(X_test[:, features["Features"]])),
+                          accuracy_score(y_test,
+                                         model2.predict(X_test[:, features["Features"]])),
+                          accuracy_score(y_test,
+                                         model3.predict(X_test[:, features["Features"]])),
+                          accuracy_score(y_test,
+                                         pd.DataFrame(model4.predict(X_test[:, features["Features"]]))\
+                                         .idxmax(axis = 1).values )],
+                        index = ["Logistic Regression", "SVC",
+                                 "Random Forest", "Neural Network"])
+        return out
+        
+        
+        
+        
+        
+        
+        
---
+++ support.py
@@ -0,0 +1,108 @@
+import numpy as np
+from scipy import stats
+from sklearn.metrics import log_loss
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from keras.callbacks import EarlyStopping
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from tensorflow.python.framework.random_seed import set_random_seed
+
+
+def likelihood_ratio_test(model1, model2, X_train1, X_train2, y_train):
+    """
+    The likelihood-ratio test assesses the goodness of fit
+    of two competing statistical models based on the ratio
+    of their likelihoods, specifically one found by
+    maximization over the entire parameter space and
+    another found after imposing some constraint.
+    
+    Parameters
+    ----------
+    
+    model1: 'sklearn.model' object (reduced)
+    model2: 'sklearn.model' object (full)
+    
+    X_train1: 'pd.DataFrame'. Training set for reduced model.
+    X_train2: 'pd.DataFrame'. Training set for full model.
+    y_train: 'pd.DataFrame'. Common to model1 and model2.
+    """    
+    ll_model1 = log_loss(y_train, model1.predict(X_train1))
+    ll_model2 = log_loss(y_train, model2.predict(X_train2))
+    
+    lambda_stat = -2*(ll_model1 - ll_model2)
+    
+    # The likelihood-ratio test rejects H0
+    # if the value of this statistic is too small
+    
+    def_freedom = X_train2.shape[1] - X_train1.shape[1]
+    
+    # The numerator corresponds to the likelihood of an observed outcome under H0.
+    # The denominator corresponds to the MAX log-likelihood of an observed outcome,
+    # varying parameters over the whole parameter space.
+    # The numerator of this ratio is less than the denominator; so, LLE in [0, 1].
+    # [!!!] Low values of the likelihood ratio mean that the observed result
+    # was much less likely to occur under the null hypothesis as compared
+    # to the alternative. High values of the statistic mean that the observed
+    # outcome was nearly as likely to occur under the null hypothesis as
+    # the alternative, and so the null hypothesis cannot be rejected.
+    
+    p_value = 1 - stats.chi2.cdf(lambda_stat, def_freedom)
+    
+    return lambda_stat, p_value
+    
+    
+def D3_pruning(X_train, y_train):
+    
+    tree = DecisionTreeClassifier(random_state = 42, criterion = "entropy")
+    tree.fit(X_train, y_train)
+
+    # 1.1 Pruning
+    path = tree.cost_complexity_pruning_path(X_train, y_train)
+    ccp_alphas, impurities = path.ccp_alphas, path.impurities
+
+    # 1.2 Cross-validation for parameters tuning
+    tree_params = {"ccp_alpha":[ccp_alphas[impurities < impty].max()
+                                for impty in np.linspace(max(0.1, impurities.min() + 0.01),
+                                                         np.round(impurities.max()/2, 3),
+                                                         num = 10)]}
+
+    tree_grid = GridSearchCV(DecisionTreeClassifier(random_state = 42, criterion = "entropy"),
+                             tree_params, cv = 3, n_jobs = -1, verbose = 0,
+                             return_train_score = True)
+    tree_grid.fit(X_train, y_train)
+    
+    return tree_grid
+
+def build_MLP(X_train, y_train_cat, features):
+    """
+    Parameters
+    ----------
+
+    X_train: output of 'prepare_data'
+    y_train_cat: 'np.ndarray'
+    features : 'dict'. output of 'variable_selection'
+    """
+    
+    set_random_seed(101)    
+    early_stopping = EarlyStopping(monitor = "val_loss", mode = "min",
+                                   patience = 5, verbose = 0)
+
+    model = Sequential(name = "ANN")
+    model.add(Dense(input_dim = len(features["Features"]),
+                    units = int(X_train.shape[1])/2,
+                    activation = "relu"))
+    model.add(Dense(units = 10, activation = "relu"))
+    model.add(Dense(units = 2, activation = "sigmoid"))
+    model.compile(loss = "binary_crossentropy", optimizer = "sgd",
+                  metrics = ["accuracy"])
+    
+    history = model.fit(X_train[:, features["Features"]], y_train_cat,
+                        epochs = 500, batch_size = min(len(X_train)/3, 50),
+                        verbose = 0, validation_split = 0.3, callbacks = [early_stopping])
+    
+    return model
+    
